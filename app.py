42428a345a2f8ca2a3c5c1c3d6c1fb1dfcd89e90
Exev0 <g_austin@live.co.uk>
2023-07-17 17:05:41 +0000

    addedbits

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+JOBS = [
+  {
+    'id': 1,
+    'title': 'Chicken Farmer',
+    'location': 'Ipswich',
+    'Salary': '£25600',
+  },
+  
+  {
+    'id': 2,
+    'title': 'chicken feeder',
+    'location': 'Ipswich',
+    'Salary': '£15600',
+  }
+]
+
+@app.route("/")
+def hello_world():
+  return render_template ('home.html', jobs=JOBS)
+
+
+if __name__ == "__main__":
+  app.run(host='0.0.0.0', debug=True)
